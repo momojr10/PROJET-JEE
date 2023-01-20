90dda7efc93118a0e1baa871b519abc7d9556b78
momojr10 <mahamadoualisidibe@yahoo.com>
2023-01-20 18:38:41 +0100

    1er Modification

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    $(".dropdown").click(function(){
+      $(".dropdown-menu").show(function(){
+        $(".dropdown").click(function(){
+          $(".dropdown-menu").hide()
+      })  
+  })
+  
+})
+}
 }
