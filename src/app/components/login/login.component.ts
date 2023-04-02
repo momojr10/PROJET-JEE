ada0512b19141305a8ab3363ff641adc7b068989
momojr10 <mahamadoualisidibe@yahoo.com>
2023-04-02 15:22:48 +0000

    security_last

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { user } from 'src/app/model/user.model';
-import { UserService } from 'src/app/services/users.service';
-import { Authentication } from 'src/app/services/authentication.service';
-import { Router } from '@angular/router';
+import { User } from 'src/app/model/User';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -13,8 +11,116 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
+  user:User[]=[] ;
+  usersSub: Subscription | undefined
+  user2: User[]=[] ;
+   ar:boolean;
+
+signinForm:FormGroup ;
+email:FormControl ;
+password: FormControl ;
+
+client: User={
+  name:'',
+  lastName:'',
+  password: '',
+};
+
+
+
+constructor(private fb:FormBuilder,private userService:UserService){
+   this.email=fb.control("",[Validators.minLength(3)])
+  this.password=fb.control("",[Validators.required,Validators.minLength(3)]) 
+  this.ar=false;
+  this.signinForm=fb.group({
+  email:this.email,
+  password:this.password
+ })
+
+ 
+
+
+
+}
+
+ngOnInit(): void {
+  
+  /*this.produitService.getAllProducts()
+  .subscribe((value:Product[])=>{
+    this.Produit=value
+   // console.log(this.Produit);
+  })*/
+this.usersSub=this.userService.getAllusers()
+  .subscribe({
+    next:(value:User[])=>{
+      this.user=value;
+       console.log(this.user);
+       
+    },
+    error:(error:any)=>{
+      console.log(error);  
+    },
+    complete:()=>{
+      console.log("Completed");
+
+
+    
+      console.log(this.user[0]);
+      console.log(this.email.value);
+      console.log(this.password.value);
+      
+      
+      
+    }
+  })
+   
+
+
 
 
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+handleSubmit(){
+
+  this.client.name=this.email.value;
+  this.client.password=this.password.value;
+  console.log(this.client.name);
+  console.log(this.client.password);
+ 
+
+
+  
+  this.userService.login(this.client).subscribe(resp=>{
+
+
+    let jwt=resp.headers.get('Authorization');  
+    console.log(jwt);
+    this.userService.saveToken(jwt);
+  
+  },err=>{
+
+
+
+  })
+ 
+
+
+
+}
+
+}
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { JwtHelperService } from "@auth0/angular-jwt";
 import { Observable } from "rxjs";
-import { user } from "../model/user.model";
+import { User } from "../model/user.model";
+
 
 
 
@@ -11,15 +13,87 @@ import { user } from "../model/user.model";
 @Injectable({providedIn:"root"})
 
 export class UserService{
+   
+   private baseUrl = 'http://localhost:3000/api/clients';
 
-  //  private listuser:string="http://localhost:8080/users";   
-   
-   
+
+  jwt:any | undefined;
+  name:string | undefined;
+  roles:Array<any> | undefined;
+
+
+
+  
+
+
+
     constructor(private http: HttpClient) { }
 
-  /*  getAllProducts():Observable<user[]>{
-        return this.http.get<any>(this.listuser);
-      }*/
+    login(data: User):Observable<any>{
+      let host="http://localhost:8080";
+      return this.http.post<User>(host+"/login",data, {observe:'response'});
+    }
+
+    getAllusers():Observable<any>{
+      let host="http://localhost:8080";
+      return this.http.get(host+"/users");
+    }
+
+    save(client:User):Observable<any>{
+      let host="http://localhost:8080";
+      return this.http.post<User>(host+"/users", client);
+    }
+
+    saveToken(jwt:string){
+      localStorage.setItem("token",jwt);
+      this.jwt=jwt;
+      this.parseJwt();
+
+    }
+
+    parseJwt(){
       
+      let jwtHelper=new JwtHelperService();
+    
+      this.name=jwtHelper.decodeToken(this.jwt)['sub'];
+     
+      this.roles=jwtHelper.decodeToken(this.jwt)['roles'];
+
+      console.log(this.name);
+      console.log(this.roles);
+      console.log(this.isAuthenticated());
+      console.log(this.isAdmin());
+      console.log(this.isUser());
+      
+    }
+
+    isAdmin(){
+       return this.roles?.indexOf('ADMIN');
+    }
+
+    isUser(){
+
+      return this.roles?.indexOf('USER');
+    }
+
+    isAuthenticated(){
+
+      return this.roles!=undefined;
+    }
+
+    loadToken(){
+      this.jwt=localStorage.getItem("token");
+      this.parseJwt();
+    }
+
+    logout(){
+      localStorage.removeItem("token");
+      this.jwt=undefined;
+      this.name=undefined;
+      this.roles=undefined;
+
+    }
+
+
 
 }
